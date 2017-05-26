406e59591813ab0eaee6f8d6fafd4bb49153519f
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-05-25 23:44:18 -0400

    Created a class to work on Types

---
+++ src/main/java/com/ocajp8/ch2/types/DataTypes.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.ocajp8.ch2.types;
+
+/**
+ * @author Yashwanth
+ *
+ */
+public class DataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char character ='1';
+		byte byteValue=1;
+		short integer =1; 
+		int intValue =1;
+		long longValue =1L;
+		float floatValue =1;
+		double doubelValue = 1;
+		boolean boolValue = false;
+		
+		System.out.println(" Character ="+character+
+				"\n ByeValue = "+byteValue+
+				"\n Short Value="+integer+
+			    "\n Interger Value ="+intValue+
+				"\n Long Value ="+longValue+
+				"\n Float Value ="+floatValue+
+				"\n Double Value ="+doubelValue+
+				"\n Boolean Value ="+boolValue);
+	}
+
+}
