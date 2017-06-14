f42e0358c989a8cded07708085afb56d280f66a3
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-06-13 23:48:59 -0400

    Updated Information Hiding with its components

--- src/main/java/com/ocajp8/ch3/methodnEncaps/MethodReturnTypes.java
+++ src/main/java/com/ocajp8/ch3/methodnEncaps/MethodReturnTypes.java
@@ -4,17 +4,71 @@
 package com.ocajp8.ch3.methodnEncaps;
 
 /**
+ * Information Hiding = Encapsulation of a Class
  * @author Yashwanth
  *
  */
 public class MethodReturnTypes {
+	
+	//Exposing Object Functionality using Public Methods
+	private int num ;
+	private double salary;
+	
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
 
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+
+	/**
+	 * 
+	 */
+	 public MethodReturnTypes() {
+		this(122);
+
+		System.out.println(" # 3 Invoking a constructor from other constructor, this() must be the first statement");
+		num =50;
+		System.out.println(" # 3 Value of Num set by default Constructor ="+num);
+	}
+ 
+	
+	public MethodReturnTypes(int num) {
+		//this();
+		System.out.println(" # 3 Value of Num set by single param Constructor ="+num);
+	}
+	
+	{
+		System.out.println(" # 2 I `m from Initializer Block- created only before calling constructor , i.e, before object creation");
+	}
+	
+	static{
+		System.out.println("# 1 I`m From Static Initializer Block, called must even without object creation");
+	}
+	
+	/**
+	 * 
+	 */
+	public void MethodReturnTypes() {
+		num =500;
+		System.out.println(" # 4 Value of Num set by Regular Method ="+num);
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("# 1 Static Method main() Executed First");
 		returnVoid();
 		returnVoid(23);
 		returnString();
@@ -23,56 +77,62 @@ public class MethodReturnTypes {
 		returnfloat();
 		returnByte();
 		returndouble();
-		System.out.println(totalCount("Yashwanth",2,1,32,123,44,22,555,77,88));
+		System.out.println(" 	"+totalCount("Yashwanth",2,1,32,123,44,22,555,77,88));
+		MethodReturnTypes method = new MethodReturnTypes();
+		method.MethodReturnTypes();
+		MethodReturnTypes method1 = new MethodReturnTypes(12333);
+	
+		
 	}
 	
 	public static int totalCount(String name ,int... nums){
 		int total=0;
-		System.out.println("Array Length ::"+name+"::Length::"+nums.length);
+		System.out.println("	Array Length ::"+name+"::Length::"+nums.length);
 		for (int i=0;i<nums.length;i++)
 			total+=nums[i];
 		return total;
 	}
 
 	public static void returnVoid(){
-		System.out.println("Void method");
+		System.out.println("	Void method");
 	}
 	public static void returnVoid(int index){
 		if(index>0)
 			return;
 		else
-			System.out.println("Void method");
+			System.out.println("	Void method");
 	}
 	
 	public static String returnString(){
-		System.out.println("String method");
+		System.out.println("	String method");
 		
 		return "String";
 	}
 	
 	public static boolean returnboolean(){
-		System.out.println("boolean method");
+		System.out.println("	boolean method");
 		return true;
 	}
 	
 	
 	public static int returnInt(){
-		System.out.println("Int method");
+		System.out.println("	Int method");
 		return 1;
 	}
 	
 	public static float returnfloat(){
-		System.out.println("float method");
+		System.out.println("	float method");
 		return 1f;
 	}
 	
 	public static byte returnByte(){
-		System.out.println("Byte method");
+		System.out.println("	Byte method");
 		return 1;
 	}
 	
 	public static double returndouble(){
-		System.out.println("double method");
+		System.out.println("	double method");
 		return 1.2;
 	}
+	
 }
