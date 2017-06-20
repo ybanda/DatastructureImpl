c1363f034d3d716cd76ce0e15c49330d4bbb7572
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-06-19 23:27:48 -0400

    Updated String Method Operations

--- src/main/java/com/ocajp8/ch4/arrays/StringMethodImpl.java
+++ src/main/java/com/ocajp8/ch4/arrays/StringMethodImpl.java
@@ -17,7 +17,7 @@ public class StringMethodImpl {
 
 		String str1 = new String("Yashwanth");
 		String str2 = new String("Yashwanth");
-		String str3 = "Yashwanth";
+		String str3 = "Yashwanth  ";
 		String str4 = "Yashwanth";
 		String str5 = new String("Yashwanth");
 		char[] charName = new char[]{'Y','a','s','h','w','a','n','t','h'};
@@ -32,7 +32,7 @@ public class StringMethodImpl {
 		System.out.println("str4 HasCode :: "+str4.hashCode());
 		System.out.println("str5 HasCode :: "+str5.hashCode());
 		System.out.println("str6 HasCode :: "+str6.hashCode());
-		System.out.println("str7 HasCode :: "+str7.hashCode());
+		//System.out.println("str7 HasCode :: "+str7.hashCode());
 
 		System.out.println("Str1Obj == Str2Obj :: returns ::"+stringValueCompare(str1, str2));
 		System.out.println("Str3Var == Str4Var :: returns ::"+stringValueCompare(str3, str4));
@@ -40,6 +40,7 @@ public class StringMethodImpl {
 		System.out.println("Yashwanth"==str3);
 		System.out.println("str5Obj == Str6Obj :: returns ::"+stringValueCompare(str5,str6));
 		
+		stringMethodOperations(str2, str3);
 	}
 	/**
 	 * 
@@ -50,6 +51,23 @@ public class StringMethodImpl {
 	public static boolean stringValueCompare(String val1 , String val2){
 		
 	return 	(val1==val2) ?true : false; 
+	}
+	
+	/**
+	 * 
+	 * @param val1
+	 * @param val2
+	 */
+	public static void stringMethodOperations(String val1 , String val2){
+		System.out.println("---------------------Query Position of Characters--------------");
+		System.out.println("For String ::"+val1+" , Character at index :: 0 "+val1.charAt(0));
+		System.out.println("For String ::"+val1+" , Index at Y::"+val1.indexOf('Y'));
+		System.out.println("For String ::"+val1+" , SubString(0,8) "+val1.substring(0,8));
+		System.out.println("---------------------Seem to modify String--------------");
+		System.out.println(val2.trim() +" val2-- Length "+val2.trim().length());
+		System.out.println("Replace a with r :: "+val2.replace('a', 'r') +":: Starts with :: Y ?"+val2.startsWith("Y")+
+				"\n Ends with h ?"+val2.endsWith("h"));
+		
 	}
 
 }
