f0f72630937ef3e01d947f347ba2dc214c1bc0bd
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-07-02 12:57:17 -0400

    Updated ArrayList Impl

--- src/main/java/com/ocajp8/ch4/arrays/CollectionsImpl.java
+++ src/main/java/com/ocajp8/ch4/arrays/CollectionsImpl.java
@@ -5,6 +5,7 @@ package com.ocajp8.ch4.arrays;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * @author Yashwanth
@@ -48,13 +49,26 @@ public class CollectionsImpl {
 		arr.remove(0);
 		printList(arr);
 		ArrayList<Object> arrClone = (ArrayList<Object>)arr.clone();
-		printList(arrClone);
+		printListWithIterator(arrClone);
 		
 		
 		System.out.println("Array :: contains :: R :: "+arr.contains("R"));
 		System.out.println("Array :: Index of :: B :: "+arr.indexOf("B")+":: Last Index of :: "+arr.lastIndexOf("B"));
 
 		System.out.println("Check Cloned ==  "+(arr==arrClone));
+		
+		ArrayList<String> myArr = new ArrayList<String>();
+		String one ="One";
+		String two = new String("Two");
+		myArr.add(one);
+		myArr.add(two);
+		ArrayList<String> urArr = myArr;
+		one.replace("O","B");
+		for (String val : myArr)
+			System.out.println(val+":");
+
+		for (String val : urArr)
+			System.out.println(val+":");
 	}
 	
 	/**
@@ -64,8 +78,21 @@ public class CollectionsImpl {
 	public static void printList(List<Object> listObj){
 		
 		System.out.println("List Capacity :: "+listObj.size());
-		for(Object obj:listObj){
+		for(Object obj:listObj)
 			System.out.println("Value in the array :: "+obj.toString());
-		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param listObj
+	 */
+	public static void printListWithIterator(List<Object> listObj){
+		
+		System.out.println("------Printing list with List Iterator----");
+		ListIterator<Object> iter = listObj.listIterator();
+		while(iter.hasNext())
+			System.out.println(iter.next());
+		
 	}
 }
